e19975b4acfa6f4d2a2995a8df2b4ba410fba71b
thegayyogay1-jpg <thegayyogay1@gmail.com>
2026-07-25 10:34:37 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 // ==========================================
 // 📌 1. ตั้งค่าตัวแปรระบบและ Global Variables
 // ==========================================
-const ADMIN_LIST = ['U1234567890abcdef1234567890abcdef']; // 👈 ใส่ LINE User ID ของแอดมิน
+const ADMIN_LIST = ['U4a0d60e9af37aa9fe66cf3e97d01cddb']; // 👈 ใส่ LINE User ID ของแอดมิน
 const COMMISSION_RATE = 0.05; // ค่าน้ำ 5% (ถ้าไม่ต้องการให้ใส่ 0)
 
 let usersWallets = {};
